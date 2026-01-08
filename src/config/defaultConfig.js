e2f6627365029cb035e1163d04a3f8550d40f830
maze92 <162896786+maze92@users.noreply.github.com>
2026-01-08 05:01:00 +0000

    Change game news check interval to 30 minutes
    Updated the game news check interval from 1 minute to 30 minutes.

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -26,7 +26,7 @@ module.exports = {
   // ==============================
   gameNews: {
     enabled: true,                  // Ativa ou desativa o sistema
-    interval: 1 * 60 * 1000,       // Intervalo de checagem em milissegundos (30 minutos)
+    interval: 30 * 60 * 1000,       // Intervalo de checagem em milissegundos (30 minutos)
     sources: [
       {
         name: "GameSpot/Reviews",                                      // Nome do feed
