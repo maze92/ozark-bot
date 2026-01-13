969e818380f9b7a706c56e1350d7e6e8928239ae
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-01-13 11:23:14 +0000

    Update User.js

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,12 +1,16 @@
 const { Schema, model } = require('mongoose');
 
-// Estrutura do usuário no MongoDB
 const userSchema = new Schema({
-  userId: { type: String, required: true },   // ID do usuário no Discord
-  guildId: { type: String, required: true },  // ID da guild
-  trust: { type: Number, default: 30 },       // Pontos de confiança (para anti-raid)
-  warnings: { type: Number, default: 0 }     // Contagem de avisos (warns)
+  userId: { type: String, required: true },
+  guildId: { type: String, required: true },
+  trust: { type: Number, default: 30 },
+  warnings: { type: Number, default: 0 },
+  warnHistory: [ // Guarda histórico de infrações
+    {
+      word: String,
+      timestamp: { type: Date, default: Date.now }
+    }
+  ]
 });
 
-// Exporta o modelo para uso no restante do bot
 module.exports = model('User', userSchema);
