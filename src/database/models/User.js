da42aa399a0508212d690be0ae1482e919916749
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-01-13 12:11:27 +0000

    Update User.js

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,16 +1,24 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-  userId: { type: String, required: true },
-  guildId: { type: String, required: true },
-  trust: { type: Number, default: 30 },
-  warnings: { type: Number, default: 0 },
-  warnHistory: [ // Guarda histórico de infrações
-    {
-      word: String,
-      timestamp: { type: Date, default: Date.now }
-    }
-  ]
-});
+  userId: {
+    type: String,
+    required: true
+  },
+  guildId: {
+    type: String,
+    required: true
+  },
+  warnings: {
+    type: Number,
+    default: 0
+  },
+  trust: {
+    type: Number,
+    default: 30
+  }
+}, { timestamps: true });
+
+userSchema.index({ userId: 1, guildId: 1 }, { unique: true });
 
 module.exports = model('User', userSchema);
