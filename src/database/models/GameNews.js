58e6292c187dc659df9f28bce23e8cdebca25755
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-01-13 14:22:24 +0000

    Update GameNews.js

--- src/database/models/GameNews.js
+++ src/database/models/GameNews.js
@@ -1,17 +1,17 @@
 const { Schema, model } = require('mongoose');
 
-// Esquema para armazenar as últimas notícias de cada feed
+// Esquema para armazenar a última notícia enviada de cada feed
 const gameNewsSchema = new Schema({
-  source: {         // Nome do feed (ex: "Polygon_PC", "IGN_PC")
+  source: {        // Nome do feed (ex: "Polygon_PC", "IGN_PC")
     type: String,
     required: true,
-    unique: true,   // Garante que cada feed tenha apenas um registro
+    unique: true    // Garante que cada feed tenha apenas um registro
   },
-  lastHash: {       // Hash da última notícia enviada
+  lastHash: {      // Hash da última notícia enviada
     type: String,
-    default: null   // null indica que ainda não foi enviada nenhuma notícia
+    default: null  // 'null' indica que ainda não foi enviada nenhuma notícia
   }
-}, { timestamps: true }); // Adiciona campos automáticos 'createdAt' e 'updatedAt'
+}, { timestamps: true }); // Adiciona createdAt e updatedAt automaticamente
 
 // Exporta o modelo para uso em gamenews.js
 module.exports = model('GameNews', gameNewsSchema);
