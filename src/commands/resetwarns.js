286aecde9090e305266d5859596e05b2618f4276
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-01-14 15:17:52 +0000

    Update resetwarns.js

--- src/commands/resetwarns.js
+++ src/commands/resetwarns.js
@@ -1,10 +1,9 @@
-const User = require('../database/models/User');
 const logger = require('../systems/logger');
-const config = require('../config/defaultConfig');
+const User = require('../database/models/User');
 
 module.exports = {
   name: 'resetwarns',
-  description: 'Reset warnings for a user',
+  description: 'Reset warnings of a user',
   allowedRoles: [
     '1385619241235120177',
     '1385619241235120174',
@@ -12,34 +11,16 @@ module.exports = {
   ],
 
   async execute(message, client, args) {
-    const member = message.mentions.members.first();
-    if (!member) {
-      return message.reply(`❌ Usage: ${config.prefix}resetwarns @user`);
-    }
+    const user = message.mentions.members.first();
+    if (!user) return message.reply('❌ Please mention a user.');
 
-    const user = await User.findOne({
-      userId: member.id,
-      guildId: message.guild.id
-    });
+    let dbUser = await User.findOne({ userId: user.id, guildId: message.guild.id });
+    if (!dbUser) dbUser = await User.create({ userId: user.id, guildId: message.guild.id, warnings: 0 });
 
-    if (!user || user.warnings === 0) {
-      return message.reply('ℹ️ This user has no warnings.');
-    }
+    dbUser.warnings = 0;
+    await dbUser.save();
 
-    user.warnings = 0;
-    await user.save();
-
-    await message.channel.send(
-      `✅ Warnings reset for **${member.user.tag}**`
-    );
-
-    await logger(
-      client,
-      'Reset Warnings',
-      member.user,
-      message.author,
-      'All warnings reset',
-      message.guild
-    );
+    await message.channel.send(`✅ Warnings for ${user.user.tag} have been reset.`);
+    await logger(client, 'Reset Warnings', user, message.author, 'Warnings reset to 0', message.guild);
   }
 };
