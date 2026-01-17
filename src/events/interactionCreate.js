697c88044317c14219c6cd8f06c747fc1df9dfba
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-01-17 09:27:38 +0000

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -6,6 +6,10 @@ const warnSlash = require('../slash/warn');
 const muteSlash = require('../slash/mute');
 const userinfoSlash = require('../slash/userinfo');
 
+const clearSlash = require('../slash/clear');
+const unmuteSlash = require('../slash/unmute');
+const helpSlash = require('../slash/help');
+
 module.exports = (client) => {
   client.on('interactionCreate', async (interaction) => {
     try {
@@ -14,17 +18,13 @@ module.exports = (client) => {
 
       const name = interaction.commandName;
 
-      if (name === 'warn') {
-        return warnSlash(client, interaction);
-      }
+      if (name === 'warn') return warnSlash(client, interaction);
+      if (name === 'mute') return muteSlash(client, interaction);
+      if (name === 'userinfo') return userinfoSlash(client, interaction);
 
-      if (name === 'mute') {
-        return muteSlash(client, interaction);
-      }
-
-      if (name === 'userinfo') {
-        return userinfoSlash(client, interaction);
-      }
+      if (name === 'clear') return clearSlash(client, interaction);
+      if (name === 'unmute') return unmuteSlash(client, interaction);
+      if (name === 'help') return helpSlash(client, interaction);
     } catch (err) {
       console.error('[interactionCreate] Error:', err);
 
